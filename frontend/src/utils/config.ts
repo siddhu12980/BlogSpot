cfbe7ec5c7dd0e0a99d9fae4ed9403685a6afdee
Siddhanta Acharya <siddhu12980@gmail.com>
2024-09-24 21:21:12 +0530

    Updated config

--- frontend/src/utils/config.ts
+++ frontend/src/utils/config.ts
@@ -1,5 +1,5 @@
 export default {
-    apiUrl: "http://localhost:8787",
+    // apiUrl: "http://localhost:8787",
 
-    // apiUrl: import.meta.env.VITE_BACKEND_URL,
+    apiUrl: import.meta.env.VITE_BACKEND_URL,
 };
