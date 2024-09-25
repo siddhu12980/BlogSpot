e9c4769b82510e37637f68957c1ba9534f5b86f6
Siddhanta Acharya <siddhu12980@gmail.com>
2024-09-25 23:19:34 +0530

    updated UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,25 +5,28 @@ import Blog from "./pages/Blog/Blog";
 import { CreateBlog } from "./pages/Blog/CreateBlog";
 import { Homepage } from "./pages/Home/Homepage";
 import { AuthorProfile } from "./pages/Author/AuthorProfile";
-import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-  export const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 function App() {
-
   return (
     <>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/blog" element={<CreateBlog />} />
-          <Route path="/" element={<Homepage />} />
-          <Route path="/author/:id" element={<AuthorProfile />} />
-        </Routes>
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Routes>
+q            {localStorage.getItem("token") === null
+              ? (window.location.href = "/signin")
+              : null}
+
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/blog/:id" element={<Blog />} />
+            <Route path="/blog" element={<CreateBlog />} />
+            <Route path="/" element={<Homepage />} />
+            <Route path="/author/:id" element={<AuthorProfile />} />
+          </Routes>
+        </BrowserRouter>
       </QueryClientProvider>
     </>
   );
