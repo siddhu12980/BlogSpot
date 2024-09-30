33f57f6ef6a4913b25c561efc1259b3484f62ad6
Siddhanta Acharya <siddhu12980@gmail.com>
2024-10-01 00:27:36 +0530

    fixed image uploader

--- frontend/src/utils/converter.ts
+++ frontend/src/utils/converter.ts
@@ -1,11 +1,16 @@
-const convertToBase64 = (file: File): Promise<string | ArrayBuffer | null> => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file); 
-      reader.onload = () => resolve(reader.result); 
-      reader.onerror = (error) => reject(error);
-    });
-  };
-  
-  export default convertToBase64;
-  
+const convertToBase64 = (file: File): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file); 
+    reader.onload = () => {
+      if (typeof reader.result === 'string') {
+        resolve(reader.result);
+      } else {
+        reject(new Error('File could not be read as base64 string.'));
+      }
+    };
+    reader.onerror = (error) => reject(error);
+  });
+};
+
+export default convertToBase64;
