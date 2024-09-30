6b94c1d496e99994dc186e4a35d08ce5f6679371
Siddhanta Acharya <siddhu12980@gmail.com>
2024-10-01 00:14:50 +0530

    updated image upload

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -78,50 +78,7 @@ app.use("/api/v1/user", authMiddleware, userRoutes)
 app.use("/api/v1/blog", authMiddleware, blogFetRouter)
 app.use("/api/v1/blog", authMiddleware, blogRouter)
 
-app.get("/upload", async (req: Request, res: Response) => {
 
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API,
-    api_secret: process.env.CLOUDINARY_SECRET
-  });
-
-  const img_url = "https://media.istockphoto.com/id/1437816897/photo/business-woman-manager-or-human-resources-portrait-for-career-success-company-we-are-hiring.jpg?s=612x612&w=0&k=20&c=tyLvtzutRh22j9GqSGI33Z4HpIwv9vL_MZw_xOE19NQ="
-
-  const uploadResult = await cloudinary.uploader
-    .upload(
-      img_url, {
-      public_id: 'shoes',
-    }
-    )
-    .catch((error) => {
-      console.log(error);
-    });
-
-  console.log("uploaded Result", uploadResult);
-
-
-  // Optimize delivery by resizing and applying auto-format and auto-quality
-  const optimizeUrl = cloudinary.url('shoes', {
-    fetch_format: 'auto',
-    quality: 'auto'
-  });
-
-  console.log(" Optimized Result", optimizeUrl);
-
-  // Transform the image: auto-crop to square aspect_ratio
-  const autoCropUrl = cloudinary.url('shoes', {
-    crop: 'auto',
-    gravity: 'auto',
-    width: 500,
-    height: 500,
-  });
-
-  console.log("Cropped Url", autoCropUrl);
-
-
-
-});
 
 
 app.listen(PORT, () => {
